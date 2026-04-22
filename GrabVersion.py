19e76d3b8b62bdba9eb520c208b3301ed542d63c
Alexander Johnson <alexjohnson2004@gmail.com>
2026-04-22 14:11:46 +0100

    Add GrabVersion to git

---
+++ GrabVersion.py
@@ -0,0 +1,32 @@
+VSVersionInfo(
+    ffi=FixedFileInfo(
+        filevers=(2026, 4, 21, 1),
+        prodvers=(2026, 4, 21, 1),
+        mask=0x3F,
+        flags=0x0,
+        OS=0x40004,
+        fileType=0x1,
+        subtype=0x0,
+        date=(0, 0),
+    ),
+    kids=[
+        StringFileInfo(
+            [
+                StringTable(
+                    "040904B0",
+                    [
+                        StringStruct("CompanyName", "TransparentBox Co.\0"),
+                        StringStruct("FileDescription", "Start Youtube-dl GUI\0"),
+                        StringStruct("FileVersion", "2026.4.21.f1 (full)\0"),
+                        StringStruct("InternalName", "DownloaderGUI\0"),
+                        StringStruct("LegalCopyright", "\xa9 TransparentBox Co. All rights reserved.\0"),
+                        StringStruct("OriginalFilename", "DownloaderGUI.Exe\0"),
+                        StringStruct("ProductName", "TransparentBox Co. Youtube-dl GUI\0"),
+                        StringStruct("ProductVersion", "2026.4.21.1\0"),
+                    ],
+                )
+            ]
+        ),
+        VarFileInfo([VarStruct("Translation", [1033, 1200])]),
+    ],
+)
